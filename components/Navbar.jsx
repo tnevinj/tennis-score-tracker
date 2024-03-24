1d48cbe05db4048ecb1549d3d1d5c5549c0a9879
Nevin Jaramba <tnevinj@gmail.com>
2024-03-24 14:44:30 +0200

    tennisscore nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
               href="/matches"
               className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
             >
-              TennisScores.co.za
+              TennisScore.co.za
             </Link>
           </div>
           <div className="hidden sm:ml-6 sm:flex sm:items-center">
@@ -30,73 +30,8 @@ const Navbar = () => {
               Add Match
             </Link>
           </div>
-          <div className="-mr-2 flex items-center sm:hidden">
-            {/* Mobile menu button */}
-            <button
-              type="button"
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
-              aria-controls="mobile-menu"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              {/* Icon when menu is closed. */}
-              <svg
-                className="block h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              {/* Icon when menu is open. */}
-              <svg
-                className="hidden h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
         </div>
       </div>
-
-      {/* Mobile menu, show/hide based on menu state. */}
-      {/* <div className="sm:hidden" id="mobile-menu">
-        <div className="pt-2 pb-3 space-y-1">
-          <Link
-            href="/matches"
-            className="block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
-          >
-            Matches
-          </Link>
-        </div>
-        <div className="pt-4 pb-3 border-t border-gray-200">
-          <div className="mt-3 space-y-1">
-            <Link
-              href="/matches/new"
-              className="block w-full px-5 py-3 text-center font-medium text-white bg-blue-600 hover:bg-blue-700"
-            >
-              Add Match
-            </Link>
-          </div>
-        </div>
-      </div> */}
     </nav>
   );
 };
